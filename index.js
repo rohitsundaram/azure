4f5507caa30304f75d9504b4d2bf3323e86d2353
rohitsundaram <rohitsundaram.95@gmail.com>
2020-07-08 21:28:46 +0530

    fourth commit

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to Azure!!');
     });
 
-    //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
+//     //PORT ENVIRONMENT VARIABLE
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => console.log(`Listening on port ${port}..`));
