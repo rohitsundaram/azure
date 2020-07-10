436a74666cae9c14b8440ce630777cc0f4f0e2f7
rohitsundaram <rohitsundaram.95@gmail.com>
2020-07-10 11:05:46 +0530

    changed

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Welcome to Azure!!');
-    });
-
-    //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
